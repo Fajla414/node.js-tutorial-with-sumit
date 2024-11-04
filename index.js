a6b7d96ad50a6e0534d9d1210625e75fde004105
Fajla Rabby <blfajlarabby414@gmail.com>
2024-11-04 22:32:59 +0600

    app.locals source code

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 const express = require("express");
-
+const handle = require("./handle");
 const app = express();
-// app.use(express.raw());      // variation of data
-// app.use(express.json());
-// app.use(express.urlencoded());
-// app.use(express.static(__dirname + "/public/"));
-// app.use(
-//   express.static(__dirname + "/public/", {
-//     index: "home.html",
-//   })
-// );
-// const router = express.Router();
-const router = express.Router({
-  caseSensitive: true,
-});
-app.use(router);
 
-app.get("/", (req, res) => {
-  res.send("this is home page");
-});
-
-router.post("/about", (req, res) => {
-  res.send("this is home page with post request");
-});
+app.locals.title = "My App"; // entire application variable
+app.get("/", handle);
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
