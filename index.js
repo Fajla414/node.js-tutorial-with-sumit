85e80a6d56b7542c1da1a64559456f39967cb5f4
Fajla Rabby <blfajlarabby414@gmail.com>
2024-11-04 06:49:52 +0600

    lesson-16 source code

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 
 const app = express();
+// app.use(express.raw());      // variation of data
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.static(__dirname + "/public/"));
+// app.use(
+//   express.static(__dirname + "/public/", {
+//     index: "home.html",
+//   })
+// );
+// const router = express.Router();
+const router = express.Router({
+  caseSensitive: true,
+});
+app.use(router);
 
 app.get("/", (req, res) => {
   res.send("this is home page");
 });
 
-app.post("/", (req, res) => {
+router.post("/about", (req, res) => {
   res.send("this is home page with post request");
 });
 
